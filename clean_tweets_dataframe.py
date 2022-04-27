ffd5f2483a9d5efe81760f18dabb626cc2129a32
Nardos Tilahun <nardostilahun83@gmail.com>
2022-04-27 23:31:42 +0300

    Unit test fix

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -20,36 +22,50 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
-        df.drop_duplicates(inplace=True) 
+        df.drop_duplicates(subset="created_at" , keep=False, inplace= True)
         
+    
         return df
-    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
-        """
-        convert column to datetime
-        """
-        df['created_at'] = pd.to_datetime(df['created_at'], format='%d%b%Y:%H:%M:%S.%f')
-        
-        df = df[df['created_at'] >= '2020-12-31' ]
-        
-        return df
+    def convert_to_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
+        """convert column to datetime."""
+        self.df['created_at'] = pd.to_datetime(
+            df['created_at'], errors='coerce')
+
+        self.df = df[df['created_at'] >= '2020-12-31']
+
+        # self.convert_to_numbers(self.df)
+        return self.df
     
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
-        """
-        convert columns like polarity, subjectivity, retweet_count
-        favorite_count etc to numbers
-        """
-        df = self.df
-        df['polarity'] = pd.to_numeric(df['polarity'])
-        df['subjectivity'] = pd.to_numeric(df['subjectivity'])
-        df['retweet_count'] = pd.to_numeric(df['retweet_count'])
-        df['favorite_count'] = pd.to_numeric(df['favorite_count'
-        
-        return df
+        """Convert columns like polarity, subjectivity, favorite_count, retweet_count to numbers."""
+        self.df['polarity'] = pd.to_numeric(
+            df['polarity'], errors='coerce')
+        self.df['retweet_count'] = pd.to_numeric(
+            df['retweet_count'], errors='coerce')
+        self.df['favorite_count'] = pd.to_numeric(
+            df['favorite_count'], errors='coerce')
+
+        # self.remove_non_english_tweets(self.df)
+        return self.df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove non english tweets from lang
         """
         
-        df = df[df['lang'] == 'en']        
-        return df
+        self.df = df.query("lang == 'en' ")
+
+
+        return self.df
+
+if __name__ == "__main__":
+    tweet_df = pd.read_csv("./processed_tweet_data.csv")
+    c = Clean_Tweets(tweet_df)
+    print(c.df.head())
+    df = c.drop_unwanted_column(c.df)
+    df = c.drop_duplicate(df)
+    df = c.convert_to_numbers(df)
+    df = c.convert_to_datetime(df)
+    df = c.remove_non_english_tweets(df)
+
+    df.to_csv('cleaned_tweet_data.csv', index=False)
--- extract_dataframe.py
+++ extract_dataframe.py
@@ -33,89 +33,102 @@ class TweetDfExtractor:
         
         self.tweets_list = tweets_list
 
+
     # an example function
-    def find_statuses_count(self)->list:
-        statuses_count 
+    # def find_statuses_count(self)->list:
+    #     statuses_count 
         
     def find_full_text(self)->list:
-        text = []
-        for i in self.tweets_list:
-           try:
-               text.append(i["retweeted_status"]["extended_tweet"]["full_text"])
-           except KeyError:
-               text.append(i["text"])
-               logger.error("Can't find full text")
-        return text
+        text = [item['text'] for item in self.tweets_list]
+        return text;
        
     
     def find_sentiments(self, text)->list:
-        
-        polarity = []
-        subjectivity = []
-
-        for each in text:
-            if (each):
-                result = TextBlob(str(each)).sentiment
-                polarity.append(result.polarity)
-                subjectivity.append(result.subjectivity)
-
+        polarity, subjectivity = [], []
+        for tweet in text:
+            blob = TextBlob(tweet)
+            sentiment = blob.sentiment
+            polarity.append(sentiment.polarity)
+            subjectivity.append(sentiment.subjectivity)
         return polarity, subjectivity
 
     def find_created_time(self)->list:
        
+        created_at = [item['created_at'] for item in self.tweets_list]
         return created_at
-
     def find_source(self)->list:
-        source = [tweet['source'] for tweet in self.tweets_list]
+        source = [item['source'] for item in self.tweets_list]
 
         return source
 
     def find_screen_name(self)->list:
-        screen_name = [tweet['user']['screen_name']
-                       if 'user' in tweet else "" for tweet in self.tweets_list]
+        screen_name = [item['user']['screen_name']
+                       if 'user' in item else '' for item in self.tweets_list]
+        return screen_name
 
     def find_followers_count(self)->list:
-        followers_count = [tweet['user']['followers_count']
-                           if 'user' in tweet else '' for tweet in self.tweets_list]
+        followers_count = [item['user']['followers_count']
+                           if 'user' in item else '' for item in self.tweets_list]
+        return followers_count
 
     def find_friends_count(self)->list:
-        friends_count =  [tweet['user']['friends_count']
-                         if 'user' in tweet else '' for tweet in self.tweets_list]
+        friends_count = [item['user']['followers_count']
+                         if 'user' in item else '' for item in self.tweets_list]
+        return friends_count
 
-    def is_sensitive(self)->list:
-        try:
-            is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
-        except KeyError:
-            is_sensitive = None
+
+
+    def is_sensitive(self) -> list:
+        """Check sensitivity of tweets.
+        """
+        is_sensitive = [data['possibly_sensitive']
+                        if 'possibly_sensitive' in data.keys() else '' for data in self.tweets_list]
 
         return is_sensitive
 
     def find_favourite_count(self)->list:
-        fav_count = [x.get('retweeted_status', {}).get('favorite_count', 0) for x in self.tweets_list]
-        return fav_count
+        favorite_count = [item['retweeted_status']['favorite_count']
+                          if 'retweeted_status' in item.keys() else 0 for item in self.tweets_list]
+
+        return favorite_count
     
-    def find_retweet_count(self)->list:
-        retweet_count = [entry['retweet_count']
-                         for entry in self.tweets_list]
+    def find_retweet_count(self) -> list:
+        """Find retweet count of each tweet.
+        """
+        retweet_count = [item['retweeted_status']['retweet_count']
+                         if 'retweeted_status' in item.keys() else 0 for item in self.tweets_list]
+        return retweet_count
 
-    def find_hashtags(self)->list:
-        hashtags =[entry['entities']['hashtags']
-                    for entry in self.tweets_list]
+    def find_hashtags(self) -> list:
+        """Find hashtags of each tweet.
+        """
+        hashtags = [item['entities']['hashtags'] if 'entities' in item.keys(
+        ) else '' for item in self.tweets_list]
+        return hashtags
 
+    def find_mentions(self) -> list:
+        """Find mentions of each tweet.
+        """
+        mentions = [item['entities']['user_mentions']
+                    if 'entities' in item.keys(
+        ) else '' for item in self.tweets_list]
+        return mentions
 
-    def find_mentions(self)->list:
-        mentions =  [entry['entities']['user_mentions']
-                    for entry in self.tweets_list]
+    def find_location(self) -> list:
+        """Find location of each tweet.
+        """
+        location = [data['user']['location']
+                    if 'user' in data.keys(
+        ) else '' for data in self.tweets_list]
 
-
-    def find_location(self)->list:
-        try:
-            location = self.tweets_list['user']['location']
-        except TypeError:
-            location = ''
-        
         return location
 
+    def find_lang(self) -> list:
+        """Find lang of tweets.
+        """
+        lang = [item['lang'] for item in self.tweets_list]
+
+        return lang
     
         
         
@@ -139,7 +152,16 @@ class TweetDfExtractor:
         hashtags = self.find_hashtags()
         mentions = self.find_mentions()
         location = self.find_location()
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+
+        items = [created_at,source,text,polarity,lang,fav_count,retweet_count,screen_name,follower_count,friends_count,sensitivity,hashtags,mentions,location]
+        
+        for i,item in enumerate(items):
+            if len(item) == 1:
+                print("empty array: ", i)
+
+        data = zip(created_at, source, text, polarity, subjectivity,
+                   lang, fav_count, retweet_count, screen_name, follower_count,
+                   friends_count, sensitivity, hashtags, mentions, location)
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
@@ -151,11 +173,11 @@ class TweetDfExtractor:
                 
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+    columns = ['created_at', 'source', 'text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json("./data/Economic_Twitter_Data.json")
     tweet = TweetDfExtractor(tweet_list)
-    tweet_df = tweet.get_tweet_df() 
+    tweet_df = tweet.get_tweet_df(True) 
 
     # use all defined functions to generate a dataframe with the specified columns above
 
