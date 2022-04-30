7722c7eb223f17d2edefb60bdf6fae22b77a0b5e
Nardos Tilahun <nardostilahun83@gmail.com>
2022-04-30 23:40:38 +0300

    Generated a Dashbord

---
+++ dashboard.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import plotly.express as px
+import re
+from wordcloud import WordCloud, STOPWORDS
+import plotly.express as px
+from utils import (remove_mentions,
+remove_hashtags,
+remove_unfinished_letters,
+remove_RT)
+
+st.title(' Dashboard ')
+
+DATA_URL = ('data\processed_tweet_data.csv')
+
+def loadData(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    # lowercase = lambda x: str(x).lower()
+    # data.rename(lowercase, axis='columns', inplace=True)
+    # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = loadData(10000)
+
+data_load_state.text('Loading data...done!')
+
+def barChart(data, title, X, Y):
+    title = title.title()
+    st.title(f'{title} Chart')
+    msgChart = (alt.Chart(data).mark_bar().encode(alt.X(f"{X}:N", sort=alt.EncodingSortField(field=f"{Y}", op="values",
+                order='ascending')), y=f"{Y}:Q"))
+    st.altair_chart(msgChart, use_container_width=True)
+
+
+
+
+
+def polarity_pie():
+    def classify_polarity(polarity):
+        polarity = float(polarity)
+        # polarity = -1 if type(polarity)==str else float(polarity)
+        if polarity < 0:
+            return "negative"
+        elif polarity > 0:
+            return "positive"
+        else:
+            return "neutral"
+    data = loadData(1000)
+    data = data[data['polarity']!="ko"]
+    # data = data[data['polarity']!="ko"]
+    data["polarity_label"] = data['polarity'].apply(lambda x: classify_polarity(x))
+    data.dropna(subset=["polarity"],inplace=True)
+    fig = px.pie(data,names="polarity_label",title ="Polarity count", width=500, height=350)
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    st.plotly_chart(fig)
+
+
+def stBarChart():
+    df = loadData(1000)
+    dfCount = pd.DataFrame({'Tweet_count': df.groupby(['original_author'])['original_text'].count()}).reset_index()
+    dfCount["original_author"] = dfCount["original_author"].astype(str)
+    dfCount = dfCount.sort_values("Tweet_count", ascending=False)
+
+    num = st.slider("Select number of Rankings", 0, 50, 5)
+    title = f"Top {num} Ranking By Number of tweets"
+    barChart(dfCount.head(num), title, "original_author", "Tweet_count")
+
+def wordCloud():
+    df = loadData(1000)
+    cleanText = ''
+    df["cleaned_text"] = df["original_text"].apply(lambda x: re.split('https:\/\/.*', str(x))[0])
+    df["cleaned_text"] = df["cleaned_text"].apply(lambda x: x.encode('ascii', 'ignore').decode('ascii'))
+    df["cleaned_text"] = df["cleaned_text"].apply(lambda x: x.replace('\n',''))
+    df["cleaned_text"] = df["cleaned_text"].apply(lambda x: remove_hashtags(x) )
+    df["cleaned_text"] = df["cleaned_text"].apply(lambda x: remove_mentions(x) )
+    df["cleaned_text"] = df["cleaned_text"].apply(lambda x: remove_unfinished_letters(x))
+    df["cleaned_text"] = df["cleaned_text"].apply(lambda x: remove_RT(x))
+    for text in df['cleaned_text']:
+        tokens = str(text).lower().split()
+
+        cleanText += " ".join(tokens) + " "
+    weird_words =  ['s', 'kur', 'u', 't', 'rt', 'ti', 'o', 'd', 'vk', 'to', 'co',
+                    'dqlw', 'z', 'nd', 'm', 'de', 'du', 'le', 'und', 'die', 'der', 'will', 'nicht']
+    stopwords = STOPWORDS.union(weird_words)
+    wc = WordCloud(stopwords=stopwords, width=650, height=450, background_color='white', min_font_size=5).generate(cleanText)
+    st.title("Tweet Text Word Cloud")
+    st.image(wc.to_array())
+
+
+
+def langPie():
+    df = data
+    dfLangCount = pd.DataFrame({'Tweet_count': df.groupby(['lang'])['original_text'].count()}).reset_index()
+    dfLangCount["lang"] = dfLangCount["lang"].astype(str)
+    dfLangCount = dfLangCount.sort_values("Tweet_count", ascending=False)
+    dfLangCount.loc[dfLangCount['Tweet_count'] < 10, 'lang'] = 'Other languages'
+    st.title(" Tweets Language pie chart")
+    fig = px.pie(dfLangCount, values='Tweet_count', names='lang', width=500, height=350)
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    colB1, colB2 = st.columns([2.5, 1])
+
+    with colB1:
+        st.plotly_chart(fig)
+    with colB2:
+        st.write(dfLangCount)
+
+st.subheader('Raw data')
+st.write(data)
+
+st.title("Data Display")
+# selectHashTag()
+st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
+# selectLocAndAuth()
+st.title("Data Visualizations")
+wordCloud()
+with st.expander("Show More Graphs"):
+    stBarChart()
+    langPie()
+    polarity_pie()
---
+++ tweet_data.py
@@ -0,0 +1,142 @@
+import os
+import pandas as pd
+import mysql.connector as mysql
+from mysql.connector import Error
+
+def DBConnect(dbName=None):
+  
+    conn = mysql.connect(host='localhost', user='root', password="PaperTown83!",
+                         database=dbName, buffered=True)
+    cur = conn.cursor()
+    return conn, cur
+
+
+def createDB(dbName: str) -> None:
+    """
+
+    Parameters
+    ----------
+    dbName :
+        str:
+    dbName :
+        str:
+    dbName:str :
+
+
+    Returns
+    -------
+
+    """
+    conn, cur = DBConnect()
+    cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
+    conn.commit()
+    cur.close()
+
+def createTables(dbName: str) -> None:
+
+    conn, cur = DBConnect(dbName)
+    sqlFile = 'twitter_schema.sql'
+    fd = open(sqlFile, 'r')
+    readSqlFile = fd.read()
+    fd.close()
+
+    sqlCommands = readSqlFile.split(';')
+
+    for command in sqlCommands:
+        try:
+            res = cur.execute(command)
+        except Exception as ex:
+            print("Command skipped: ", command)
+            print(ex)
+    conn.commit()
+    cur.close()
+
+    return
+
+def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    df :
+        pd.DataFrame:
+    df :
+        pd.DataFrame:
+    df:pd.DataFrame :
+
+
+    Returns
+    -------
+
+    """
+    cols_2_drop = ['Unnamed: 0', 'timestamp', 'sentiment', 'possibly_sensitive', 'cleaned_text']
+    try:
+        df = df.drop(columns=cols_2_drop, axis=1)
+        df = df.fillna(0)
+    except KeyError as e:
+        print("Error:", e)
+
+    return df
+
+
+def insert_tweet(dbName: str, df: pd.DataFrame, table_name: str) -> None:
+   
+    conn, cur = DBConnect(dbName)
+
+    df = preprocess_df(df)
+
+    for _, row in df.iterrows():
+        sqlQuery = f"""INSERT INTO {table_name} (created_at, source, cleaned_text, polarity, subjectivity, lang,
+                    favorite_count, retweet_count, original_author, followers_count, friends_count,
+                    hashtags, user_mentions, place)
+             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+        data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11],
+                row[12], row[13])
+
+        try:
+            # Execute the SQL command
+            cur.execute(sqlQuery, data)
+            # Commit your changes in the database
+            conn.commit()
+            print("Inserted the tweets")
+        except Exception as e:
+            conn.rollback()
+            print("Error: ", e)
+    return
+
+def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs) -> pd.DataFrame:
+   
+    connection, cursor1 = DBConnect(**kwargs)
+    if many:
+        cursor1.executemany(*args)
+    else:
+        cursor1.execute(*args)
+
+    # get column names
+    field_names = [i[0] for i in cursor1.description]
+
+    # get column values
+    res = cursor1.fetchall()
+
+    # get row count and show info
+    nrow = cursor1.rowcount
+    if tablename:
+        print(f"{nrow} recrods fetched from {tablename} table")
+
+    cursor1.close()
+    connection.close()
+
+    # return result
+    if rdf:
+        return pd.DataFrame(res, columns=field_names)
+    else:
+        return res
+
+
+if __name__ == "__main__":
+    createDB(dbName='tweets')
+    createTables(dbName='tweets')
+
+    df = pd.read_csv('cleaned_tweet.csv')
+
+    insert_tweet(dbName='tweets', df=df, table_name='TweetInformation')
